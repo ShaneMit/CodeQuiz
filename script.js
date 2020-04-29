87531e3860a9a2510b80f5bf97c3eed8f71d5ca6
ShaneMit <shanemit@cox.net>
2020-04-29 00:37:13 -0700

    created filler questions

--- script.js
+++ script.js
@@ -14,12 +14,39 @@ function nextQuestion () {
 // Array for questions
 const questions = [
   {
-    question: 'Filler text for question1 ?'
+    question: 'Filler text for question1?'
     answers: [
       {text: 'a', correct: true},
       {text: 'b', correct: false},
-      
-        
+      {text: 'c', correct: false},
+      {text: 'd', correct: false}
+    ]
+  }
+    {
+    question: 'Filler text for question2?'
+    answers: [
+      { text: 'a', correct: false },
+      { text: 'b', correct: false },
+      { text: 'c', correct: true },
+      { text: 'd', correct: false }
+    ]
+  }
+    {
+    question: 'Filler text for question3?'
+    answers: [
+      { text: 'a', correct: false },
+      { text: 'b', correct: false },
+      { text: 'c', correct: false },
+      { text: 'd', correct: true }
+    ]
+  }
+    {
+    question: 'Filler text for question4?'
+    answers: [
+      { text: 'a', correct: false },
+      { text: 'b', correct: true },
+      { text: 'c', correct: false },
+      { text: 'd', correct: false }
     ]
   }
 ]
